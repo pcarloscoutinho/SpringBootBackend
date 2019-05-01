8709f459fd95c032e8b1d8ff701edfc02c797694
pcarloscoutinho <p.carloscoutinho@gmail.com>
2019-05-01 14:03:11 -0300

    ItemPedido and ItemPedidoPK

--- src/main/java/com/paulocoutinho/cursospring/CursoSpringApplication.java
+++ src/main/java/com/paulocoutinho/cursospring/CursoSpringApplication.java
@@ -13,6 +13,7 @@ import com.paulocoutinho.cursospring.domain.Cidade;
 import com.paulocoutinho.cursospring.domain.Cliente;
 import com.paulocoutinho.cursospring.domain.Endereco;
 import com.paulocoutinho.cursospring.domain.Estado;
+import com.paulocoutinho.cursospring.domain.ItemPedido;
 import com.paulocoutinho.cursospring.domain.Pagamento;
 import com.paulocoutinho.cursospring.domain.PagamentoComBoleto;
 import com.paulocoutinho.cursospring.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.paulocoutinho.cursospring.repositories.CidadeRepository;
 import com.paulocoutinho.cursospring.repositories.ClienteRepository;
 import com.paulocoutinho.cursospring.repositories.EnderecoRepository;
 import com.paulocoutinho.cursospring.repositories.EstadoRepository;
+import com.paulocoutinho.cursospring.repositories.ItemPedidoRepository;
 import com.paulocoutinho.cursospring.repositories.PagamentoRepository;
 import com.paulocoutinho.cursospring.repositories.PedidoRepository;
 import com.paulocoutinho.cursospring.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class CursoSpringApplication implements CommandLineRunner{
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
@@ -115,7 +119,18 @@ public class CursoSpringApplication implements CommandLineRunner{
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
 		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 		
 	}
 
---
+++ src/main/java/com/paulocoutinho/cursospring/repositories/ItemPedidoRepository.java
@@ -0,0 +1,14 @@
+package com.paulocoutinho.cursospring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.paulocoutinho.cursospring.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+	
+	
+	
+
+}
